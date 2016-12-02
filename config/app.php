7c1773e55b11373a7b2aa60a31e62188f65f640b
Lucas Pires <lucas.pires.mattos@gmail.com>
2016-12-02 10:44:56 -0200

    lara modules

--- config/app.php
+++ config/app.php
@@ -166,8 +166,7 @@ return [
         /*
          * Package Service Providers...
          */
-
-        //
+        Nwidart\Modules\LaravelModulesServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -225,6 +224,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Module' => Nwidart\Modules\Facades\Module::class,
 
     ],
 
